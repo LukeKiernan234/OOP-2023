8a30306d66ca6de8e591960b7aef6cc2304c5a11
lukeKiernan <lukekiernan8@gmail.com>
2023-02-13 11:34:59 +0000

    commit message

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,27 +5,101 @@ import processing.core.PApplet;
 public class Loops extends PApplet {
 
 	int mode = 0;
+	int x = 10;
+	int y = 10;
+	int diameter = 50;
+	int gap = 10;
+
 	public void settings() {
 		size(1000, 1000);
 	}
 
 	public void setup() {
+		colourmode(HSB);
 	}
 
 
-
 	public void keyPressed() {
-		
 		mode = key - '0';
 		println(mode);
 	}
 
 	public void draw() {
-		background(0);
-		fill(255);
-		
-		
-		
+    background(0);
+    fill(255);
+	noStroke();
 
+    switch (mode) {
+      case 0:
+	  	int no_of_circles = 10;
+		float d = width / (float) no_of_circles;
+		for (int j = 0; j < no_of_circles; j++) {
+
+        for (int i = 0; i < no_of_circles; i++) {
+			float x = (d * 0.5f) + (d * i);
+			float y = (d * 0.5f) + (d * j);
+			float c = (i+j) / ((no_of_circles -1) * 2.0f) * 255.0f;	
+			fill(c, 255, 255);
+			circle(x, y, d);
+        }
 	}
+		x =0;
+
+		  y=0;
+        break;
+
+      case 1:
+        break;
+
+      default:
+        break;
+    }
+  }
 }
+/*
+ * 
+ * package ie.tudublin;
+ * 
+ * import processing.core.PApplet;
+ * 
+ * public class Loops extends PApplet {
+ * 
+ * int mode = 0;
+ * public void settings() {
+ * size(1000, 1000);
+ * }
+ * 
+ * public void setup() {
+ * }
+ * 
+ * 
+ * 
+ * public void keyPressed() {
+ * 
+ * mode = key - '0';
+ * println(mode);
+ * }
+ * 
+ * public void draw() {
+ * background(0);
+ * fill(255);
+ * 
+ * switch (mode){
+ * case 0:
+ * 
+ * break;
+ * 
+ * case 1:
+ * 
+ * break;
+ * 
+ * default:
+ * break;
+ * }
+ * 
+ * 
+ * 
+ * 
+ * }
+ * }
+ */
